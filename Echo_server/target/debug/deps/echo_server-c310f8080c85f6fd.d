a03f752e9e73f53522159b58b5750a852230ea77
solaryssky <kuzdima2@gmail.com>
2022-08-08 21:17:09 +0500

    add target folder
    add target
    
    Co-Authored-By: Red Elvises <kuzdima2@gmail.com>

---
+++ Echo_server/target/debug/deps/echo_server-c310f8080c85f6fd.d
@@ -0,0 +1,5 @@
+/home/dima/Rust/echo_server/target/debug/deps/echo_server-c310f8080c85f6fd: src/main.rs
+
+/home/dima/Rust/echo_server/target/debug/deps/echo_server-c310f8080c85f6fd.d: src/main.rs
+
+src/main.rs:
---
+++ Echo_server/target/debug/echo_server.d
@@ -0,0 +1,1 @@
+/home/dima/Rust/echo_server/target/debug/echo_server: /home/dima/Rust/echo_server/src/main.rs
